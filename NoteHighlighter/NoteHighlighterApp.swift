1ac286815dc51192fa2d2fc9e57725b0d7c26876
khoasdyn <khoasdyn@gmail.com>
2026-03-04 09:29:56 +0700

    Initial Commit

---
+++ NoteHighlighter/NoteHighlighterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteHighlighterApp.swift
+//  NoteHighlighter
+//
+//  Created by khoasdyn on 4/3/26.
+//
+
+import SwiftUI
+
+@main
+struct NoteHighlighterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
